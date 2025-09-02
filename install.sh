ebf8582057bbcffd141210448c7c915d310e4ae6
Enzo PRUDENT <enzoprudent@gmail.com>
2025-09-02 16:09:06 +0200

    [LGDH] Port

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ cat <<EOL > ./config/config.json
     "CLIENT_ID":"your-client-id",
     "GUILD_ID":"your-guild-id",
 
-    "PORT":8080,
+    "PORT":80,
 
     "DB_HOST":"localhost",
     "DB_PORT":3306,
